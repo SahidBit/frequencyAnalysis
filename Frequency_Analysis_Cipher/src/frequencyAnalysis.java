2cf2f0e2df93453d5e8454497e3dc4a290f5c6f3
SahidBit <52377434+SahidBit@users.noreply.github.com>
2021-05-30 15:03:56 +0300

    Creating the variable for the cipher frequency

--- Frequency_Analysis_Cipher/src/frequencyAnalysis.java
+++ Frequency_Analysis_Cipher/src/frequencyAnalysis.java
@@ -1,4 +1,49 @@
+	
+	public class frequencyAnalysis{
+	
+	
 
-public class frequencyAnalysis {
+	    static String cipher = "";
+	
+
+	    public static void frequency_letter(String str) {
+	
+
+	        int[] freq = new int[str.length()];
+	        int i, j;
+	
+
+	
+
+	        char string[] = str.toCharArray();
+	
+
+	        for (i = 0; i < str.length(); i++) {
+	            freq[i] = 1;
+	            for (j = i + 1; j < str.length(); j++) {
+	                if (string[i] == string[j]) {
+	                    freq[i]++;
+	
+
+	
+
+	                    string[j] = '0';
+	                }
+	            }
+	        }
+	
+
+	
+
+	        for (i = 0; i < freq.length; i++) {
+	            if (string[i] != ' ' && string[i] != '0') {
+	                System.out.print(string[i] + "->" + freq[i] + ",");
+	            }
+	        }
+	    }
 
 }
+	
+
+		
+
