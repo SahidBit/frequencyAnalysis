c62cba7159a3ab0585819468e1b50f2f05d17261
SahidBit <52377434+SahidBit@users.noreply.github.com>
2021-05-30 22:12:13 +0300

    Trying to replace and analysis text

--- Frequency_Analysis_Cipher/src/frequencyAnalysis.java
+++ Frequency_Analysis_Cipher/src/frequencyAnalysis.java
@@ -1,3 +1,5 @@
+	import java.util.Scanner;
+	import java.io.File;
 	
 	public class frequencyAnalysis{
 	
@@ -40,10 +42,71 @@
 	                System.out.print(string[i] + "->" + freq[i] + ",");
 	            }
 	        }
-	    }
+	    
 
-}
+        for (i = 0; i < freq.length; i++) {
+            
+			if (string[i] != ' ' && string[i] != '0') {
+                System.out.print(string[i] + "->" + freq[i] + ",");
+			}
+			
+        }
+        
+        
+	 }
+        
+        
+          
+ 
+
+
+    public static void replace_Text(String str) {
+        str = str.toLowerCase();
+        cipher = cipher.toLowerCase();
+        String[] txt = str.split(",");
+
+
+        for (int i = 0; i < txt.length; i++) {
+
+
+            String[] txt_replace = txt[i].split(":");
+            cipher = cipher.replace(txt_replace[0], txt_replace[1]);
+   
+         
+
+        System.out.println("Plain Text :" + cipher + "");
+        
+          
+        }
+   
+    }
 	
 
-		
+
+    public static void main(String[] args) {
+
+
+        try {
+
+
+            System.out.println("Cipher text :");
+            if (!args[0].equals("")) {
+                File myObj = new File(args[0]);
+                
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    cipher += data;
+                    System.out.println(data);
+                }
+                myReader.close();
+
+
+                System.out.println("Analysis :");
+                
+                frequency_letter(cipher);
+                
+                System.out.println("");
+  
+            }
 
